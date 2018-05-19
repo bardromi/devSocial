32be71f3ef474044d7394a1c2c51a5adc8674a81
Bar <bardromi@gmail.com>
2018-05-19 13:35:19 +0300

    Added jwt auth, next ill add passport

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://bar:bar@ds227740.mlab.com:27740/devconnector'
+    mongoURI: 'mongodb://bar:bar@ds227740.mlab.com:27740/devconnector',
+    secretOrKey: 'secret'
 };
