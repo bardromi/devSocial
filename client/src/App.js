8d97e5b6cbb35a87011dd6edfa487327cead6513
Bar Dromi <bardromi@gmail.com>
2018-09-09 16:18:00 +0300

    Profile:  - Created main profile page  - Created header component

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,8 @@ import CreateProfile from './components/craete-profile/CreateProfile';
 import EditProfile from './components/edit-profile/EditProfile';
 import AddExperience from './components/add-credentials/AddExperience';
 import AddEducation from './components/add-credentials/AddEducation';
-
+import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
 
 import './App.css';
 
@@ -57,6 +58,8 @@ class App extends Component {
                         <div className="container">
                             <Route exact path="/register" component={Register}/>
                             <Route exact path="/login" component={Login}/>
+                            <Route exact path="/profiles" component={Profiles}/>
+                            <Route exact path="/profile/:handle" component={Profile}/>
                             <Switch>
                                 <PrivateRoute
                                     exact
