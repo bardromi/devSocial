16020d261ea8174640d02210c2c8efce4b1d4204
bar <bardromi@gmail.com>
2018-09-20 18:35:07 +0300

    fixed server.js from process.enc to process.env

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
 // Serve static assets if in production
-if (process.enc.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     // Set static folder
     app.use(express.static('client/build'));
 
